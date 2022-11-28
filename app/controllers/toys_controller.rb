300de0f33bd9f3c86c1aeb6eb1b363741b356bb3
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-11-28 06:50:31 +0300

    completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,31 +1,48 @@
 class ToysController < ApplicationController
-  wrap_parameters format: []
 
-  def index
-    toys = Toy.all
-    render json: toys
-  end
-
-  def create
-    toy = Toys.create(toy_params)
-    render json: toy, status: :created
-  end
-
-  def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
-  end
-
-  def destroy
-    toy = Toy.find_by(id: params[:id])
-    toy.destroy
-    head :no_content
-  end
-
-  private
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   
-  def toy_params
-    params.permit(:name, :image, :likes)
+    wrap_parameters format: []
+  
+    def index
+      toys = Toy.all
+      render json: toys
+    end
+  
+    def create
+      toy = Toy.create!(toy_params)
+      render json: toy, status: :created
+    end
+  
+    def update
+      toy = find_toy
+      toy.update!(toy_params)
+      render json: toy, status: :ok
+    end
+  
+    def destroy
+      toy = find_toy
+      toy.destroy
+      head :no_content
+    end
+  
+    private
+    
+    def toy_params
+      params.permit(:name, :image, :likes)
+    end
+  
+    def find_toy
+      Toy.find(params[:id])
+    end
+  
+    def render_unprocessable_entity_response(exception)
+      render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+  
+    def render_not_found_response(exception)
+      render json: { error: "#{exception.model} not found" }, status: :not_found
+    end
+  
   end
-
-end
